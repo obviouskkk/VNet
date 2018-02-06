d5a160b22e21c6e32af41c65f9dfc0e5b440e174
zzy <942744575@qq.com>
2018-02-06 23:09:43 +0800

    调整结构

---
+++ src/Net.hpp
@@ -0,0 +1,20 @@
+/* ***********************************************************************
+
+  > File Name: Net.hpp
+  > Author: zzy
+  > Mail: 942744575@qq.com 
+  > Created Time: Tue 06 Feb 2018 10:15:38 PM CST
+
+ ********************************************************************** */
+#ifndef __Net_HPP__
+#define __Net_HPP__
+
+
+class Net
+{
+public:
+    int net_send();
+};
+
+
+#endif   // __Net_HPP__

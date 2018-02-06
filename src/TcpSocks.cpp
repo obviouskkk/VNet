5cb7ec20594e594fd2130b58940a3486ccd23661
zzy <942744575@qq.com>
2018-02-06 14:59:44 +0800

    new TcpSocks.cpp

---
+++ src/TcpSocks.cpp
@@ -0,0 +1,22 @@
+/* ***********************************************************************
+
+  > File Name: TcpSocks.cpp
+  > Author: zzy
+  > Mail: 942744575@qq.com 
+  > Created Time: Tue 06 Feb 2018 02:31:35 PM CST
+
+ ********************************************************************** */
+
+#include "TcpSocks.hpp"
+
+/*
+int TcpSock::Send(const std::string & data, size_t len){
+
+
+}
+*/
+
+int TcpSock::Connect(const char* ip, const uint16_t port){
+    return 0;
+    
+}
